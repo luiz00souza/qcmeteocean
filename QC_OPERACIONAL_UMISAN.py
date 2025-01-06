f983570105dde85dc7cea00ca8bffa9a565cd615
luiz00souza <106404356+luiz00souza@users.noreply.github.com>
2025-01-06 14:43:24 -0300

    Update QC_OPERACIONAL_UMISAN.py

--- QC_OPERACIONAL_UMISAN.py
+++ QC_OPERACIONAL_UMISAN.py
@@ -1,7 +1,7 @@
 #%%IMPORTAÇÃO DAS BIBLIOTECAS
 
 import sys
-sys.path.append(r'G:\Drives compartilhados\DHE_REPASSE\2024\ID00_PD_MITR-QCMO\01_Scripts\20241206_Version_008')  # Caminho onde o arquivo .py está
+#sys.path.append(r'G:\Drives compartilhados\DHE_REPASSE\2024\ID00_PD_MITR-QCMO\01_Scripts\20241206_Version_008')  # Caminho onde o arquivo .py está
 from QC_FLAGS_UMISAN import *
 
 
