2e0c68accab33e1f15c67663145d4de08210eea1
luiz00souza <106404356+luiz00souza@users.noreply.github.com>
2025-02-07 12:53:56 -0300

    Update CPEStemporeal.py

--- CPEStemporeal.py
+++ CPEStemporeal.py
@@ -10,9 +10,8 @@ def carregar_dados():
     df = importar_dados_servidor_ftp()
     df = df.drop(columns=["Sea_Level_filtered"])
 
-    # df = df.iloc[255:]
     df['TIMESTAMP_ORIGINAL'] = pd.to_datetime(df['TIMESTAMP'])
-    # df= df.resample('30T').mean()
+    df= df.set_index('TIMESTAMP_ORIGINAL')
     df.columns = parameter_columns_ondas_nao_direcionais
     for coluna in df.columns:
         df[f'Flag_{coluna}'] = 0
